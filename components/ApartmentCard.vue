41b2bd559cc35d66133a7a39d440a029787b37d6
vxncetxn <thevancetan@gmail.com>
2022-12-12 15:35:17 +0800

    Added ApartmentCard component with dynamic base props

---
+++ components/ApartmentCard.vue
@@ -0,0 +1,28 @@
+<script setup>
+const props = defineProps(["title", "address", "area", "numItems"]);
+</script>
+
+<template>
+  <div class="px-32 py-16 bg-white hover:bg-teal-50">
+    <div class="flex gap-x-32">
+      <img
+        class="w-[240px] aspect-[3/2] rounded-8"
+        src="sample-image-1.webp"
+        alt="sample image 1"
+      />
+      <div class="flex flex-col">
+        <p class="font-sans text-24 capsize">{{ title }}</p>
+        <p class="font-sans text-18 capsize text-[#7A7A7A] mt-16">
+          {{ address }}
+        </p>
+        <div
+          class="flex font-sans text-18 capsize text-[#7A7A7A] mt-auto space-x-8"
+        >
+          <p>{{ area.sqft }} sqft / {{ area.sqm }} sqm</p>
+          <p>·</p>
+          <p>{{ numItems }} items</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -2,11 +2,41 @@
 
 <template>
   <Searchbar />
-  <div class="grid grid-cols-2">
-    <div class="h-[2000px]"></div>
+  <div class="flex">
+    <div class="flex flex-col p-32 shrink-0">
+      <p class="font-sans text-20 text-[#7A7A7A] pl-32">
+        Displaying 108 apartments
+      </p>
+      <div class="flex flex-col mt-16 gap-y-16">
+        <ApartmentCard
+          title="4-Room HDB at Bukit Timah"
+          address="383 Bukit Timah Rd #19-07, S(310856)"
+          :area="{ sqft: 1001, sqm: 93 }"
+          numItems="12"
+        />
+        <ApartmentCard
+          title="1 Room in Condo at Geylang"
+          address="64 Mattar Rd #04-43, S(322468)"
+          :area="{ sqft: 215, sqm: 20 }"
+          numItems="8"
+        />
+        <ApartmentCard
+          title="4-Room HDB at Serangoon"
+          address="2 Serangoon Gardens #06-33, S(300461)"
+          :area="{ sqft: 1205, sqm: 112 }"
+          numItems="20"
+        />
+        <ApartmentCard
+          title="5-Room HDB at Choa Chu Kang"
+          address="46 Choa Chu Kang Loop #24-02, S(311899)"
+          :area="{ sqft: 1260, sqm: 117 }"
+          numItems="17"
+        />
+      </div>
+    </div>
     <div class="sticky h-[calc(100vh-144px)] top-[144px]">
       <img
-        class="w-full h-full object-cover"
+        class="object-cover w-full h-full"
         src="/map-placeholder.png"
         alt="placeholder for map component"
       />
