82c960d348739405afb7c9c2cac6999e78f56c12
vxncetxn <thevancetan@gmail.com>
2022-12-12 16:06:34 +0800

    Added mock data as data.json along with directory for mock images; Updated data structure to be more precise and ApartmentCard to use new data with new structure; Updated page to use new mock data

--- components/ApartmentCard.vue
+++ components/ApartmentCard.vue
@@ -1,5 +1,11 @@
 <script setup>
-const props = defineProps(["title", "address", "area", "numItems"]);
+import { computed } from "vue";
+
+const props = defineProps(["apartment"]);
+
+const title = computed(() => {
+  return `${props.apartment.numRooms}-Room ${props.apartment.propertyType} at ${props.apartment.address.locale}`;
+});
 </script>
 
 <template>
@@ -7,20 +13,24 @@ const props = defineProps(["title", "address", "area", "numItems"]);
     <div class="flex gap-x-32">
       <img
         class="w-[240px] aspect-[3/2] rounded-8"
-        src="sample-image-1.webp"
-        alt="sample image 1"
+        :src="`/mock-images/${apartment.image}.webp`"
+        :alt="`Cover image for ${title}`"
       />
       <div class="flex flex-col">
-        <p class="font-sans text-24 capsize">{{ title }}</p>
+        <p class="font-sans text-24 capsize">
+          {{ title }}
+        </p>
         <p class="font-sans text-18 capsize text-[#7A7A7A] mt-16">
-          {{ address }}
+          {{ apartment.address.block }} {{ apartment.address.street }} #{{
+            apartment.address.floor
+          }}-{{ apartment.address.unit }}, S({{ apartment.address.postalCode }})
         </p>
         <div
           class="flex font-sans text-18 capsize text-[#7A7A7A] mt-auto space-x-8"
         >
-          <p>{{ area.sqft }} sqft / {{ area.sqm }} sqm</p>
+          <p>{{ apartment.area.sqft }} sqft / {{ apartment.area.sqm }} sqm</p>
           <p>·</p>
-          <p>{{ numItems }} items</p>
+          <p>{{ apartment.items }} items</p>
         </div>
       </div>
     </div>
--- pages/index.vue
+++ pages/index.vue
@@ -1,38 +1,19 @@
-<script setup></script>
+<script setup>
+import data from "../data.json";
+</script>
 
 <template>
   <Searchbar />
   <div class="flex">
     <div class="flex flex-col p-32 shrink-0">
       <p class="font-sans text-20 text-[#7A7A7A] pl-32">
-        Displaying 108 apartments
+        Displaying {{ data.length }} apartments
       </p>
-      <div class="flex flex-col mt-16 gap-y-16">
-        <ApartmentCard
-          title="4-Room HDB at Bukit Timah"
-          address="383 Bukit Timah Rd #19-07, S(310856)"
-          :area="{ sqft: 1001, sqm: 93 }"
-          numItems="12"
-        />
-        <ApartmentCard
-          title="1 Room in Condo at Geylang"
-          address="64 Mattar Rd #04-43, S(322468)"
-          :area="{ sqft: 215, sqm: 20 }"
-          numItems="8"
-        />
-        <ApartmentCard
-          title="4-Room HDB at Serangoon"
-          address="2 Serangoon Gardens #06-33, S(300461)"
-          :area="{ sqft: 1205, sqm: 112 }"
-          numItems="20"
-        />
-        <ApartmentCard
-          title="5-Room HDB at Choa Chu Kang"
-          address="46 Choa Chu Kang Loop #24-02, S(311899)"
-          :area="{ sqft: 1260, sqm: 117 }"
-          numItems="17"
-        />
-      </div>
+      <ul class="flex flex-col mt-16 gap-y-16">
+        <li v-for="apartment in data" :key="apartment.id">
+          <ApartmentCard :apartment="apartment" />
+        </li>
+      </ul>
     </div>
     <div class="sticky h-[calc(100vh-144px)] top-[144px]">
       <img
