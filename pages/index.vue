616be61be9416c19a20e8c82a6e4e7913c0d63ef
vxncetxn <thevancetan@gmail.com>
2022-12-12 09:28:30 +0800

    Added image placeholder for map component

--- pages/index.vue
+++ pages/index.vue
@@ -3,9 +3,13 @@
 <template>
   <Searchbar />
   <div class="grid grid-cols-2">
-    <div class="h-[2000px] border border-blue-500">Hello</div>
-    <div
-      class="sticky border border-red-500 h-[calc(100vh-144px)] top-[144px]"
-    ></div>
+    <div class="h-[2000px]"></div>
+    <div class="sticky h-[calc(100vh-144px)] top-[144px]">
+      <img
+        class="w-full h-full object-cover"
+        src="/map-placeholder.png"
+        alt="placeholder for map component"
+      />
+    </div>
   </div>
 </template>
