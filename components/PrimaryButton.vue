c2a537502fcb895fb20e22bf941b83f7cbe58074
vxncetxn <thevancetan@gmail.com>
2022-12-13 19:18:02 +0800

    Added svg loading mechanism via vite-svg-loader; Added PrimaryButton component; Built most of Searchbar component

---
+++ components/PrimaryButton.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+  <button
+    class="p-16 font-sans text-white bg-black text-20 capsize rounded-8 hover:bg-teal-800"
+  >
+    <slot></slot>
+  </button>
+</template>
--- components/Searchbar.vue
+++ components/Searchbar.vue
@@ -1,5 +1,16 @@
+<script setup>
+import MagnifyingGlassIcon from "~/assets/icons/magnifying-glass.svg";
+</script>
+
 <template>
   <div
-    class="sticky top-80 z-10 w-screen h-64 border-b border-[#EBEBEB] px-64 bg-white"
-  ></div>
+    class="flex items-center gap-x-24 sticky top-80 z-10 w-screen h-64 border-b border-[#EBEBEB] px-64 bg-white focus-within:shadow-lg"
+  >
+    <MagnifyingGlassIcon class="w-24 h-24" />
+    <input
+      class="w-full font-sans text-black text-20 capsize focus:outline-none"
+      placeholder="Search location, house type, etc."
+    />
+    <PrimaryButton>Search</PrimaryButton>
+  </div>
 </template>
