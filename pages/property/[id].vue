a311438914d85c6b1bd4a5ddac6c8f8ab94f13e1
vxncetxn <thevancetan@gmail.com>
2022-12-15 14:27:09 +0800

    Added max-w Container component

--- pages/property/[id].vue
+++ pages/property/[id].vue
@@ -23,86 +23,89 @@ const fullAddress = computed(() => {
 
 <template>
   <SecondaryBar />
-  <div class="flex flex-col px-16 py-48 gap-y-24 sm:px-40 lg:px-64">
-    <HeaderOne>{{ title }}</HeaderOne>
-    <Text size="lg" color="grey">{{ fullAddress }}</Text>
-    <div class="flex flex-col mt-40 lg:flex-row gap-x-32">
-      <div class="flex flex-col w-full lg:w-3/5 gap-y-48">
-        <img
-          class="object-cover w-full rounded-4"
-          :src="`/mock-images/${property.image}`"
-          :alt="`Cover image for ${title}`"
-        />
-        <div class="flex gap-x-24">
-          <Avatar
-            :src="property.owner.image"
-            :alt="`Profile image of ${property.owner.name}`"
+  <Container
+    ><div class="flex flex-col px-16 py-48 gap-y-24 sm:px-40 lg:px-64">
+      <HeaderOne>{{ title }}</HeaderOne>
+      <Text size="lg" color="grey">{{ fullAddress }}</Text>
+      <div class="flex flex-col mt-40 lg:flex-row gap-x-32">
+        <div class="flex flex-col w-full lg:w-3/5 gap-y-48">
+          <img
+            class="object-cover w-full rounded-4"
+            :src="`/mock-images/${property.image}`"
+            :alt="`Cover image for ${title}`"
           />
-          <div class="flex flex-col gap-y-16">
-            <Text size="lg">Owned by {{ property.owner.name }}</Text>
-            <div class="flex flex-wrap gap-x-8 gap-y-12">
-              <Text color="grey">{{
-                property.isWholeHouse ? "Whole House" : "Single Room"
-              }}</Text>
-              <Text color="grey">·</Text>
-              <Text color="grey"
-                >{{ property.area.sqft }} sqft /
-                {{ property.area.sqm }} sqm</Text
-              >
-              <Text color="grey">·</Text>
-              <Text color="grey">{{
-                property.items.length > 1
-                  ? `${property.items.length} items`
-                  : property.items.length === 1
-                  ? "1 item"
-                  : "No items"
-              }}</Text>
+          <div class="flex gap-x-24">
+            <Avatar
+              :src="property.owner.image"
+              :alt="`Profile image of ${property.owner.name}`"
+            />
+            <div class="flex flex-col gap-y-16">
+              <Text size="lg">Owned by {{ property.owner.name }}</Text>
+              <div class="flex flex-wrap gap-x-8 gap-y-12">
+                <Text color="grey">{{
+                  property.isWholeHouse ? "Whole House" : "Single Room"
+                }}</Text>
+                <Text color="grey">·</Text>
+                <Text color="grey"
+                  >{{ property.area.sqft }} sqft /
+                  {{ property.area.sqm }} sqm</Text
+                >
+                <Text color="grey">·</Text>
+                <Text color="grey">{{
+                  property.items.length > 1
+                    ? `${property.items.length} items`
+                    : property.items.length === 1
+                    ? "1 item"
+                    : "No items"
+                }}</Text>
+              </div>
             </div>
           </div>
+          <Divider />
+          <HeaderTwo>Inventory</HeaderTwo>
+          <InventoryView :inventory="property.items" />
         </div>
-        <Divider />
-        <HeaderTwo>Inventory</HeaderTwo>
-        <InventoryView :inventory="property.items" />
+        <div
+          class="flex flex-col gap-y-40 mt-96 lg:mt-0 sticky top-[calc(var(--bars-h-lg)+32px)] w-full lg:w-2/5 bg-neutral-50 h-[calc(100vh-var(--bars-h-lg)-64px)] rounded-4 border border-neutral-200 px-24 sm:px-32 lg:px-40 py-32"
+        >
+          <div class="flex items-end gap-x-12">
+            <p class="font-serif text-40 lg:text-48 capsize">$2178</p>
+            <Text size="lg" color="grey">per month*</Text>
+          </div>
+          <ul class="flex flex-col gap-y-24">
+            <BulletListItem
+              ><Text
+                >Lease term of <span class="underline">2 years</span></Text
+              ></BulletListItem
+            >
+            <BulletListItem
+              ><Text
+                >Security deposit of
+                <span class="underline">2 months</span></Text
+              ></BulletListItem
+            >
+            <BulletListItem
+              ><Text
+                >Diplomatic clause with notice period of
+                <span class="underline">60 days</span></Text
+              ></BulletListItem
+            >
+            <BulletListItem
+              ><Text
+                >Maintenance included, liable
+                <span class="underline">up till $150</span></Text
+              ></BulletListItem
+            >
+          </ul>
+          <div class="flex flex-col items-end mt-auto text-right gap-y-20">
+            <PrimaryButton>Make enquiry</PrimaryButton
+            ><Text size="sm" color="grey"
+              >Listed fee is before any additional charges such as utilities and
+              stamp fees*</Text
+            >
+          </div>
+        </div>
       </div>
-      <div
-        class="flex flex-col gap-y-40 mt-96 lg:mt-0 sticky top-[calc(var(--bars-h-lg)+32px)] w-full lg:w-2/5 bg-neutral-50 h-[calc(100vh-var(--bars-h-lg)-64px)] rounded-4 border border-neutral-200 px-24 sm:px-32 lg:px-40 py-32"
-      >
-        <div class="flex items-end gap-x-12">
-          <p class="font-serif text-40 lg:text-48 capsize">$2178</p>
-          <Text size="lg" color="grey">per month*</Text>
-        </div>
-        <ul class="flex flex-col gap-y-24">
-          <BulletListItem
-            ><Text
-              >Lease term of <span class="underline">2 years</span></Text
-            ></BulletListItem
-          >
-          <BulletListItem
-            ><Text
-              >Security deposit of <span class="underline">2 months</span></Text
-            ></BulletListItem
-          >
-          <BulletListItem
-            ><Text
-              >Diplomatic clause with notice period of
-              <span class="underline">60 days</span></Text
-            ></BulletListItem
-          >
-          <BulletListItem
-            ><Text
-              >Maintenance included, liable
-              <span class="underline">up till $150</span></Text
-            ></BulletListItem
-          >
-        </ul>
-        <div class="flex flex-col items-end mt-auto text-right gap-y-20">
-          <PrimaryButton>Make enquiry</PrimaryButton
-          ><Text size="sm" color="grey"
-            >Listed fee is before any additional charges such as utilities and
-            stamp fees*</Text
-          >
-        </div>
-      </div>
-    </div>
-  </div>
+    </div></Container
+  >
 </template>
