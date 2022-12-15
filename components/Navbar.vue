84e70c5317f22dedb7656cc1790b3b9eab8e15ba
vxncetxn <thevancetan@gmail.com>
2022-12-15 15:45:49 +0800

    Added Navbar logo animation and link

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -12,11 +12,18 @@
         <CustomLink to="/">About</CustomLink>
       </li>
     </ul>
-    <div class="justify-self-center">
-      <svg class="w-24 h-24 sm:w-32 lg:w-40 sm:h-32 lg:h-40">
+    <NuxtLink class="relative flex group justify-self-center" to="/">
+      <svg
+        class="w-24 h-[31px] transition-transform duration-[200ms] ease-[cubic-bezier(0.45,0,0.55,1)] sm:w-32 lg:w-40 sm:h-[36px] lg:h-40 group-hover:-translate-x-[34px] group-hover:sm:-translate-x-[44px] group-hover:lg:-translate-x-[56px]"
+      >
         <use href="#rently-logomark" />
       </svg>
-    </div>
+      <svg
+        class="absolute bottom-0 -left-24 opacity-0 w-[60px] h-[27px] sm:w-[83px] lg:w-[106px] sm:h-[32px] lg:h-[35px] group-hover:opacity-100 group-hover:translate-x-20 transition-all duration-[200ms] ease-[cubic-bezier(0.45,0,0.55,1)]"
+      >
+        <use href="#rently-wordmark" />
+      </svg>
+    </NuxtLink>
     <div class="justify-self-end">
       <CustomLink to="/">Login</CustomLink>
     </div>
