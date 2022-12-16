feea7e0b44ffdbeb7ecee27be15cbbbabdb89467
vxncetxn <thevancetan@gmail.com>
2022-12-16 23:54:10 +0800

    Added proper SVG arrow to Select

--- components/Select.vue
+++ components/Select.vue
@@ -22,15 +22,24 @@ const inputHandler = (e) => {
       :for="name"
       >{{ label }}</label
     >
-    <select
-      class="px-16 py-12 font-sans border text-16 sm:text-18 lg:text-20 capsize bg-neutral-50 border-neutral-200 rounded-8"
-      :name="name"
-      :id="name"
-      :required="required"
-      :value="modelValue"
-      @input="inputHandler"
-    >
-      <slot></slot>
-    </select>
+    <div class="relative">
+      <select
+        class="w-full px-16 py-12 font-sans border text-16 sm:text-18 lg:text-20 capsize bg-neutral-50 border-neutral-200 rounded-8"
+        :name="name"
+        :id="name"
+        :required="required"
+        :value="modelValue"
+        @input="inputHandler"
+      >
+        <slot></slot>
+      </select>
+      <div
+        class="absolute top-px right-px flex items-center justify-center w-48 h-[calc(100%-2px)] bg-neutral-50 rounded-8 pointer-events-none"
+      >
+        <svg class="w-24 h-24">
+          <use href="#chevron-down-icon" />
+        </svg>
+      </div>
+    </div>
   </div>
 </template>
